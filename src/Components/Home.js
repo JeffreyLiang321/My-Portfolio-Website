589a9f75c9a71a83e3ac532e45b8400b92608119
JeffreyL420 <jeffreyliang1234@163.com>
2024-09-20 19:08:50 -0400

    Changes to Resume

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,7 +4,7 @@
 import ExperienceHeader from "./ExperienceHeader";
 const Resume = () => {
     const openResume = () => {
-        window.open(`${process.env.PUBLIC_URL}/data/Jeffrey_Liang_Resume.pdf`, '_blank');
+        window.open(`${process.env.PUBLIC_URL}/data/Jeffrey_Liang_Resume_Recent.pdf`, '_blank');
       };
     
     return (
@@ -13,10 +13,10 @@ const Resume = () => {
             <img src={`${process.env.PUBLIC_URL}/data/headshot.jpeg`} alt="image?" />
             <div className="text-box">
                 <h2>Welcome To My Website</h2>
-                <p> Hi everyone, I'm Jeffrey Liang. I'm a current freshmen studying Computer Science and Economics at Brown University. 
+                <p> Hi everyone, I'm Jeffrey Liang. I'm a current sophmore studying Computer Science and Economics at Brown University. 
                 I'm interested in UI/UX interface of website and mobile applications, fullstack, and machine learning specifically for 
-                prediction models. Besides computer science, I am involved in BTCG(Brown Technology Consulting Club) and am currently 
-                involved in a pitch for a business model for the Dojo Entreprenuership Program. 
+                prediction models. Besides computer science, I am involved in BTCG(Brown Technology Consulting Club) and completed a
+                pitch for a business model for the Dojo Entreprenuership Program. 
                 Thanks for viewing my portfolio website! </p>
                 <button className = "btn" onClick={openResume}> Download CV </button>
             </div>
